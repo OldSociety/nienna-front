3f39b95acf882422ad73f7fa60ceefa00cae9007
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2023-07-19 16:55:17 -0700

    nav-bar

--- src/Core/Header.tsx
+++ src/Core/Header.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
+import React from 'react'
 
 function Header() {
   return (
     <header>
-        <h1>Nienna Counseling</h1>
-        <h4>—— Full Stack Engineer ——</h4>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Nienna Counseling
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Our Services
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Meet the Team
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Insurance
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Book Now
+            </button>
+          </form>
+        </div>
+      </nav>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
