d3617f7f261ae837f22e1cdb8cf3ef889318aa5f
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2023-07-19 18:04:55 -0700

    image

--- src/Components/HeroModule2.tsx
+++ src/Components/HeroModule2.tsx
@@ -3,9 +3,16 @@ import React from 'react'
 function HeroModule2() {
   return (
     <div className="">
-      <div className="row">
-      <div className="col-6 welcome-img">Half</div>
-      <div className="col-6 welcome-img bg-dark">Block</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 welcome-img">
+            <img
+              src={require('../Static/Clinician.jpg')}
+              alt="clinician working"
+            />
+          </div>
+          <div className="col-6 welcome-img bg-dark">Block</div>
+        </div>
       </div>
     </div>
   )
